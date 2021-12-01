f35c15d6918021189d5901b8c48df65918104380
Izabele Almeida <izabele.almeida.1@hotmail.com>
2021-12-01 00:52:22 -0300

    Commit com exercicio do modulo 12 realizado

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,12 +11,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        //fazendo login usando fixture
+        cy.fixture('perfil').then(dados => {
+
+            cy.get('#username').type(dados.usuario) // pegando o campo de username do site e incluindo dados nele.
+            cy.get('#password').type(dados.senha, {log:false}) // pegando o campo de senha do site e incluindo dados nele.
+            cy.get('.woocommerce-form > .button').click() //mapeado o botão de login, comando para clicar no botão.
+        
+            cy.get('#primary-menu > .menu-item-629 > a').click()
+            cy.addProdutos('Abominable Hoodie', 'S', 'Green' ,1) //adicionando produtos ao carrinho usando comando customizado
+            cy.get('.woocommerce-message > .button').click() //acessando carrinho de compras
+            cy.get('.checkout-button').click() //finalizando compra
+            cy.get('#terms').click() //aceitando os termos de condicoes
+            cy.get('#order_comments').type('Embalar para presente') //incluindo uma observação ao pedido
+            cy.get('#place_order').click() //finalizando compra
+
+            cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
+        
+        })
+
+
     });
 
 
-})
+});
